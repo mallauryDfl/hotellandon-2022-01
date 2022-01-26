cfc085c3daeea7674d0af34667d9187416d23392
Mallo <mallo.vautrin@live.fr>
2022-01-26 15:17:01 +0100

    HotelLandon Mallo

--- HotelLandon.MvcRazor/Views/Customers/Edit.cshtml.cs
+++ HotelLandon.MvcRazor/Views/Customers/Edit.cshtml.cs
@@ -1,16 +1,54 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using HotelLandon.Models;
+using HotelLandon.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace HotelLandon.MvcRazor.Views.Customers
+namespace HotelLandon.MvcRazor.Pages.Rooms
 {
-    public class Index1Model : PageModel
+    public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositoryBase<Room> repository;
+
+        public EditModel(IRepositoryBase<Room> repository)
         {
+            this.repository = repository;
+        }
+
+        [BindProperty]
+        public Room Room { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || !id.HasValue)
+            {
+                return NotFound();
+            }
+
+            Room = await repository.GetAsync(id.Value);
+
+            if (Room == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await repository.UpdateAsync(Room, Room.Id);
+
+
+            return RedirectToPage("./Rooms/Index");
         }
     }
 }
--- HotelLandon.MvcRazor/Views/Customers/delete.cshtml.cs
+++ HotelLandon.MvcRazor/Views/Customers/delete.cshtml.cs
@@ -1,16 +1,50 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using HotelLandon.Models;
+using HotelLandon.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace HotelLandon.MvcRazor.Views.Customers
+namespace HotelLandon.MvcRazor.Pages.Rooms
 {
-    public class Index1Model : PageModel
+    public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositoryBase<Room> repository;
+
+        public DeleteModel(IRepositoryBase<Room> repository)
         {
+            this.repository = repository;
+        }
+
+        [BindProperty]
+        public Room Room { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Room = await repository.GetAsync(id.Value);
+
+            if (Room == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            await repository.DeleteAsync(id.Value);
+
+            return RedirectToPage("./Index");
         }
     }
 }
